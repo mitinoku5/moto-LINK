6decc63db780a848ae529d4a6857245db0ea1c2d
mitinoku5 <rarmy0225@gmail.com>
2021-09-14 22:50:32 +0900

    new page create

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php 
-  echo 1+2;
-  ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="オートバイ好きのみんなで交流できるソーシャルネットワーキングサービスです">
+  <title>moto LINK</title>
+</head>
+<body>
+  <section class="explanation">
+  <h1>moto LINK</h1>
+  <h2>へようこそ！ ここはオートバイ好きのみんなで交流できるサービスです！<br>アカウントにログインをしてみんなと繋がろう！</h2>
+  <a href="login.php">ログイン</a>
+  <p>アカウントをお持ちでない方は下の新規アカウント作成から</p>
+  <a href="register.php">新規アカウント作成</a>
+  </section>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="オートバイ好きのみんなで交流できるソーシャルネットワーキングサービスです">
+  <title>moto LINK</title>
+</head>
+<body>
+  <section class="explanation">
+  <h1>ログイン</h1>
+ 
+  
+  
+  <a href="index.php">戻る</a>
+  </section>
+</body>
+</html>
