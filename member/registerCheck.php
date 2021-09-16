804b3e79444534108661ce055a86db2a9939f83d
mitinoku5 <rarmy0225@gmail.com>
2021-09-16 22:53:34 +0900

    create registerCheck page

---
+++ member/registerCheck.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>moto LINK</title>
+
+  <link rel="stylesheet" href="../../style/css"/>
+</head>
+
+<body>
+<div id="wrap">
+  <div id="head">
+    <h1>アカウント作成</h1>
+  </div>
+  <div id="content">
+    <form action="" method="post">
+      <dl>
+        <dt>ニックネーム</dt>
+        <dd>
+        </dd>
+        <dt>メールアドレス</dt>
+        <dd></dd>
+        <dt>パスワード</dt>
+        <dd>
+          【表示されません】
+        </dd>
+        <dt>写真など</dt>
+        <dd>
+        </dd>
+      </dl>
+      <div><a href="register.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input
+		type="submit" value="登録する" /></div>
+    </form>
+  </div>
+</div>
+</body>
+</html>
